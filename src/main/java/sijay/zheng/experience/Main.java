b71a640c6789b9066a93c536ab1e6ed78dd1891a
SijayZheng <sijayzheng@gmail.com>
2023-04-18 22:05:34 +0800

    更新

--- src/main/java/sijay/zheng/experience/Main.java
+++ src/main/java/sijay/zheng/experience/Main.java
@@ -1,5 +1,7 @@
 package sijay.zheng.experience;
 
+import com.alibaba.fastjson2.JSON;
+
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,6 +15,21 @@ public class Main {
         GLOBAL.info("helllo");
 
         System.out.println(test());
+
+        List<Integer> strings = new ArrayList<>();
+        strings.add(1);
+        strings.add(2);
+        strings.add(3);
+        strings.add(4);
+        strings.add(5);
+        strings.add(6);
+        strings.add(7);
+        Iterator<Integer> iterator = strings.iterator();
+        //iterator.forEachRemaining(System.out::println);
+        iterator.next();
+        iterator.remove();
+        System.out.println(JSON.toJSONString(strings));
+
     }
 
     static String test() {
