5b2f772b44985a9b923077d017780010f1da71b8
SijayZheng <sijayzheng@gmail.com>
2023-11-27 22:15:18 +0800

    update

---
+++ da/src/test/java/sijay/zheng/da/DaApplicationTests.java
@@ -0,0 +1,13 @@
+package sijay.zheng.da;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- doc/DBeaver.md
+++
@@ -1,7 +0,0 @@
-## 安装
-
-- 通过 https://dbeaver.io/ 社区下载安装
-- 通过 MicrosoftStore下载安装
-
-
-
