a9015bc7af14df1b6153077c5202aaeb49413b79
sijay <sijayzheng@163.com>
2024-01-04 17:46:18 +0800

    update

---
+++ app/src/test/java/sijay/zheng/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package sijay.zheng.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
