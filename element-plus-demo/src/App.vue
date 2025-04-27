02e607968f5c15ad733ffa96709f675432983e39
sijay <sijayzheng@163.com>
2025-04-27 22:15:38 +0800

    update

--- element-plus-demo/src/App.vue
+++ element-plus-demo/src/App.vue
@@ -1,44 +1,70 @@
 <template>
-  <div>
-    <el-table :data="tableData" :span-method="objectSpanMethod1" border style="width: 100%; margin-top: 20px">
-      <el-table-column prop="name" label="Name" />
-      <el-table-column prop="gender" label="gender" />
-      <el-table-column prop="amount" label="Amount" />
-    </el-table>
+  <div style="display: flex">
+    <div style="text-align: center">
+      <el-button circle type="primary" @click="value=value<100?0:value-100">
+        <el-icon>
+          <arrow-left-bold/>
+        </el-icon>
+      </el-button>
+    </div>
+    <div style="width: 1600px">
+      <el-scrollbar ref="scrollbarRef" always height="32px" @scroll="scroll">
+        <div ref="areaBarRef" class="area-bar">
+          <div v-for="item in 20" :key="item" class="area-item">
+            Item {{ item }}
+          </div>
+        </div>
+      </el-scrollbar>
+    </div>
+    <div style="text-align: center">
+      <el-button circle type="primary" @click="value=value>max-100?max:value+100">
+        <el-icon>
+          <arrow-right-bold/>
+        </el-icon>
+      </el-button>
+    </div>
   </div>
-
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
-import { getSpanArr } from './utils'
+const max = ref(0)
+const value = ref(0)
+const areaBarContainerBar = ref()
+const areaBarRef = ref()
+const scrollbarRef = ref()
 
-const tableData = ref([
-  { name: 'Tom', gender: '男', amount: '234' },
-  { name: 'Tom', gender: '男', amount: '165' },
-  { name: 'Tom', gender: '男', amount: '324' },
-  { name: 'jane', gender: '女', amount: '621' },
-  { name: 'jane', gender: '女', amount: '539' },
-])
-const spanArr = ['name', 'gender']
-const spanMapper = {}
 onMounted(() => {
-  spanArr.forEach((key) => {
-    spanMapper[key] = getSpanArr(tableData.value, key)
-  })
-  console.log(spanMapper)
+  max.value = areaBarRef.value.clientWidth - 1600
+})
+watch(value, (val) => {
+  scrollbarRef.value.setScrollLeft(val)
 })
 
-const objectSpanMethod1 = ({ column, rowIndex }) => {
-  for (const key in spanMapper) {
-    if (column.property === key) {
-      const _row = spanMapper[key][rowIndex]
-      const _col = _row > 0 ? 1 : 0
-      return {
-        rowspan: _row,
-        colspan: _col,
-      }
-    }
-  }
+const inputSlider = (value) => {
+  scrollbarRef.value.setScrollLeft(value)
 }
+const scroll = ({scrollLeft}) => {
+  value.value = scrollLeft
+}
+
 </script>
+<style>
+.area-bar {
+  display: flex;
+  width: fit-content;
+  transition: transform 0.3s ease;
+}
+
+.area-bar .area-item {
+  min-width: 100px;
+  height: 24px;
+  box-sizing: border-box;
+  margin: 0 5px;
+  border: 1px solid #ccc;
+  background-color: #fff;
+}
+
+.el-icon {
+  font-size: 16px;
+}
+</style>
--- element-plus-demo/src/main.js
+++ element-plus-demo/src/main.js
@@ -1,8 +1,9 @@
 import ElementPlus from 'element-plus'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import './styles/element/index.scss'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
 
@@ -11,4 +12,8 @@ app.use(ElementPlus, {
   zIndex: 3000,
   locale: zhCn
 })
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 app.mount('#app')
